b4d4228458682c2a1af827ae12aba1dda0a01139
rabbitkg <rabbitkgbd@gmail.com>
2026-03-31 20:40:28 +0600

    added selected cart total price and button.

--- src/components/homepage/products/selectedProduct/SelectedProduct.jsx
+++ src/components/homepage/products/selectedProduct/SelectedProduct.jsx
@@ -18,19 +18,23 @@ const SelectedProduct = ({ selectedProducts, setSelectedProducts, setCount, coun
     return (
         <div>
 
-            <div className='space-y-5 border-3 border-[#62738241] rounded-2xl p-6 mb-28'> 
+            <div className='space-y-5 border-3 border-[#62738241] rounded-2xl p-6 mb-28'>
                 <h3 className='font-bold text-3xl text-left'>Your Cart</h3>
                 {
                     selectedProducts.length === 0 ?
-                    <div className='h-[350px] flex items-center justify-center flex-col gap-4'>
-                        <FiShoppingCart size={100} className='text-[#62738273]'/>
-                        <p className='text-[#627382] font-medium'>Your Cart is Empty</p>
-                    </div>
-                    : selectedProducts.map((product, ind) => {
-                        return <SelectedCard key={ind} product={product}  handleDeleteSelectedProduct={handleDeleteSelectedProduct}/>
-                    })
+                        <div className='h-[350px] flex items-center justify-center flex-col gap-4'>
+                            <FiShoppingCart size={100} className='text-[#62738273]' />
+                            <p className='text-[#627382] font-medium'>Your Cart is Empty</p>
+                        </div>
+                        : selectedProducts.map((product, ind) => {
+                            return <SelectedCard key={ind} product={product} handleDeleteSelectedProduct={handleDeleteSelectedProduct} />
+                        })
                 }
+
+                
             </div>
+
+
 
 
         </div>
--- src/components/ui/SelectedCard.jsx
+++ src/components/ui/SelectedCard.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 
-const SelectedCard = ({product, handleDeleteSelectedProduct}) => {
+const SelectedCard = ({ product, handleDeleteSelectedProduct }) => {
     return (
         <div>
             <div className='flex items-center gap-6 justify-between p-10 rounded-2xl bg-[#e6e9ee60]'>
-                            <div className='flex items-center gap-6'>
-                                <div className='border border-[#62738241] rounded-full p-3.5 bg-white'>
-                                    <img className="h-12 w-12 " src={product.productImg
-                                    } alt="product.name" />
-                                </div>
-                                <div className='text-left'>
-                                    <h2 className='flex font-semibold text-2xl mb-2'>{product.name}</h2>
-                                    <p className='text-[#627382] font-medium'>${product.price}</p>
-                                </div>
-                            </div>
-                            <button className='font-bold text-[#FF3980] cursor-pointer hover:bg-red-100 rounded-sm px-3 py-1' onClick={() => handleDeleteSelectedProduct(product)}>
-                                Remove
-                            </button>
+                <div className='flex items-center gap-6'>
+                    <div className='border border-[#62738241] rounded-full p-3.5 bg-white'>
+                        <img className="h-12 w-12 " src={product.productImg
+                        } alt="product.name" />
+                    </div>
+                    <div className='text-left'>
+                        <h2 className='flex font-semibold text-2xl mb-2'>{product.name}</h2>
+                        <p className='text-[#627382] font-medium'>${product.price}</p>
+                    </div>
+                </div>
+                <button className='font-bold text-[#FF3980] cursor-pointer hover:bg-red-100 rounded-sm px-3 py-1' onClick={() => handleDeleteSelectedProduct(product)}>
+                    Remove
+                </button>
 
-                        </div>
+            </div>
+
+            <div>
+                <div className='flex justify-between mt-5 mb-3'>
+                    <div className='text-[#627382] text-xl'>
+                        Total
+                    </div>
+                    <div className='text-2xl font-bold'>
+                        $0
+                    </div>
+                </div>
+                <button className='btn w-full max-w-6xl h-14 rounded-full text-white font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA]'>
+                    Proceed To Checkout
+                </button>
+            </div>
         </div>
     );
 };
